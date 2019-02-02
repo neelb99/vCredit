eac6ed355edc10945adecd8d68e426c20db97b12
neelb99 <neelbajaj1234@hotmail.com>
2019-02-02 18:21:20 +0530

    local db connected

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+engine = create_engine("postgresql://postgres:1677@localhost/vcredit")
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
