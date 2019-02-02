e161d50328a42529a0027f60f2de560db576f028
neelb99 <neelbajaj1234@hotmail.com>
2019-02-03 00:58:23 +0530

    added session integration

--- app.py
+++ app.py
@@ -1,87 +1,116 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from user import user
+import os
 
 app = Flask(__name__)
+#session key
+app.secret_key = os.urandom(24)
 
+#database initializing
 engine = create_engine("postgresql://postgres:1677@localhost/vcredit")
 db = scoped_session(sessionmaker(bind=engine))
 
+#home page
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if 'rollno' in session:
+        if session["admin"]:
+            adminstatus=True
+        else:
+            adminstatus=False
+        return render_template('index.html', logged=True, isadmin=adminstatus)
+    else:
+        return render_template('index.html', logged=False, isadmin=False)
 
+#register page
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if 'rollno' in session:
+        return redirect('/account')
     if request.method == "GET":
-        return render_template('register.html')
+        return render_template('register.html', alreadyexists=False)
     else:
-        roll = request.form.get("roll")
+        roll = request.form.get("roll").strip()
         check = db.execute("SELECT * from users where roll = :rollno", {"rollno": roll.upper()}).fetchone()
-        password = request.form.get("pass")
+        password = request.form.get("pass").strip()
         if not check:
-            name = request.form.get("name")
-            currentuser = user(roll.upper(), name, password)
+            name = request.form.get("name").strip()
             db.execute("insert into users (roll,name,password,balance) VALUES(:roll,:name,:pass,:balance)",
-                       {"roll": currentuser.roll, "name": currentuser.name, "pass": currentuser.password,
-                        "balance": currentuser.balance})
+                       {"roll": roll.upper(), "name": name, "pass": password,
+                        "balance": 0})
             db.commit()
-            return render_template('account.html', getname=currentuser.name, getroll=currentuser.roll,
-                                   getbal=currentuser.balance)
+            session['rollno'] = roll.upper()
+            session['admin']=False
         else:
-            if check.password == password:
-                return render_template('account.html', getname=check.name, getroll=check.roll,
-                                       getbal=check.balance)
-            else:
-                return render_template('login.html', wrong=True)
+            return render_template('register.html',alreadyexists=True)
 
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if 'rollno' in session:
+        return redirect('/account')
     if request.method == "GET":
         return render_template('login.html',wrong=False)
     else:
-        roll = request.form.get("roll")
+        roll = request.form.get("roll").strip()
         check = db.execute("SELECT * from users where roll = :rollno", {"rollno": roll.upper()}).fetchone()
-        password = request.form.get("pass")
         if not check:
             return render_template('login.html', wrong=True)
         else:
+            password = request.form.get("pass").strip()
             if check.password == password:
-                return render_template('account.html', getname=check.name, getroll=check.roll,
-                                       getbal=check.balance)
+                session['rollno']=roll.upper()
+                if session['rollno']=='ADMIN':
+                    session['admin']=True
+                else:
+                    session['admin']=False
+                return redirect('/account')
             else:
                 return render_template('login.html', wrong=True)
 
 @app.route("/account")
 def account():
-    return redirect('/login')
+    if 'rollno' in session:
+        user = db.execute("SELECT * from users where roll = :rollno", {"rollno": session['rollno']}).fetchone()
+        return render_template('account.html',getname=user.name,getroll=user.roll,getbal=user.balance, isadmin=session['admin'])
+    else:
+        return redirect('/login')
 
 @app.route("/pay")
 def pay():
-    return render_template('pay.html')
+    if 'rollno' in session:
+        return render_template('pay.html', loggedout=False)
+    else:
+        return render_template('pay.html', loggedout=True)
 
 @app.route("/verify",methods=["POST","GET"])
 def verify():
     if request.method=="GET":
         return redirect('/pay')
     else:
-        roll = request.form.get("roll")
+        if 'rollno' in session:
+            roll = session['rollno']
+        else:
+            roll = request.form.get("roll").strip()
         check = db.execute("SELECT * from users where roll = :rollno", {"rollno": roll.upper()}).fetchone()
         if not check:
             return redirect('/pay')
         else:
-            password = request.form.get("pass")
+            if 'rollno' in session:
+                user = db.execute("SELECT * from users where roll = :rollno", {"rollno": session['rollno']}).fetchone()
+                password = user.password
+            else:
+                password = request.form.get("pass").strip()
             if check.password != password:
                 return redirect('/pay')
             else:
-                receiver = request.form.get("receiver")
+                receiver = request.form.get("receiver").strip()
                 check2 = db.execute("SELECT * from users where roll = :rollno", {"rollno": receiver}).fetchone()
                 if not check2:
                     return redirect('/pay')
                 else:
-                    amount = request.form.get("amount")
+                    amount = request.form.get("amount").strip()
                     if int(amount)>check.balance:
                         return redirect('/pay')
                     else:
@@ -89,3 +118,44 @@ def verify():
                         db.execute("update users set balance = :newbal2 where roll = :rollno", {"newbal2":check2.balance+int(amount),"rollno": receiver})
                         db.commit()
                         return render_template('verify.html',payer=check.name,receiver=check2.name, amount=amount)
+
+@app.route("/logout")
+def logout():
+    if 'rollno' in session:
+        session.pop('rollno',None)
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route("/update")
+def update():
+    if 'rollno' in session:
+        if session['admin']:
+            return render_template('update.html')
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route("/updateverify",methods=["GET","POST"])
+def updateverify():
+    if 'rollno' in session:
+        if session['admin']:
+            password = request.form.get("pass").strip()
+            admin = db.execute("Select * from users where roll='ADMIN'").fetchone()
+            if password == admin.password:
+                amount = request.form.get("amount").strip()
+                roll = request.form.get("roll").strip()
+                user =  db.execute("Select * from users where roll=:rollno",{"rollno":roll}).fetchone()
+                if user:
+                    db.execute("update users set balance=:newbal where roll=:rollno",{"rollno":roll,"newbal":user.balance+int(amount)})
+                    db.commit()
+                    return render_template('updateverify.html',amount=amount, receiver=user.name)
+                else:
+                    return redirect('/update')
+            else:
+                return redirect('/update')
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
--- user.py
+++
@@ -1,8 +0,0 @@
-class user:
-    def __init__(self,entroll,entname,entpass):
-        self.roll = entroll
-        self.name = entname
-        self.password = entpass
-        self.balance = 0
-
-
